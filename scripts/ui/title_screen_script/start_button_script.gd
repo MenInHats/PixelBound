7c28ef081b33008cdecbde7c6be371da87c5d9c7
eg0975887 <197536179+MenInHats@users.noreply.github.com>
2025-10-17 10:09:37 -0500

    Small Update - Improved Start Button
    Changed Scene:
    - start_button, this button is being reworked to now include a sprite change when the mouse cursor hovers over the button
    Added Assets:
    -StartGameMenu, A placeholder/finalized version the the start game button
    -StartGameMenu Alt, A alternate version of the start game button for when the cursor is hovering over button

--- scripts/ui/title_screen_script/start_button_script.gd
+++ scripts/ui/title_screen_script/start_button_script.gd
@@ -3,7 +3,24 @@ extends Area2D
 # Optional sound call, when button press
 @export var sfx_button_press = Node2D
 @export var particle_button_press = Node2D
+#@onready var sprite = $
+# Load the two textures
+var normal_texture = preload("res://assets/ui/StartGameMenu.png")
+var hover_texture = preload("res://assets/ui/StartGameMenu Alt.png")
+
+#func _ready():
+	## Connect button signals
+	#sprite.mouse_entered.connect(_on_mouse_entered)
+	#sprite.mouse_exited.connect(_on_mouse_exited)
 
 func _input_event(viewport: Viewport, event: 	InputEvent, shape_idx: int):
 	if event is InputEventMouseButton and event.pressed:
 		SceneManager.change_scene("res://scenes/test/test_area.tscn")
+		
+
+
+#func _on_mouse_entered():
+	#sprite.texture = hover_texture
+#
+#func _on_mouse_exited():
+	#sprite.texture = normal_texture
